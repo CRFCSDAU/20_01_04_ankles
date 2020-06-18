8d1ae2be9918b41cf524ea71cb6f65b327cda1d6
DarrenDahly <darrendl1@gmail.com>
2020-06-19 00:09:36 +0100

    data cleaning update

---
+++ scripts/00_project.R
@@ -0,0 +1,78 @@
+
+# Overview -----
+
+
+
+
+
+
+
+# Notes from investigator June 15, 2020
+
+# I`m sorry for the delay in sending the data for analysis, the last patient was
+# recruited in CUH on Thursday 11th, and I received their data late on Friday.
+# We have recruited 160 patients in total, 140 from UHW and 20 from CUH. Our
+# planned number was 140, but CUH started recruitment two months ago, and we had
+# to introduce a new block of 20 to accommodate CUH.
+#
+# I have attached the data folder, the first section in yellow include patients
+# characteristics, hospital stay and complications rate. This is our main focus
+# for the coming annual review.
+#
+# Each follow-up point has a different colour starting from 2 weeks to 1 year.
+# Our primary outcome is OMAS score at six weeks, if you have the time it will
+# be great to include OMAS and SF-36 for two weeks and six weeks follow up. As
+# the data is incomplete, we can do available case analysis rather than
+# imputation this time if that makes things easier!. The deadline for annual
+# review submission is Friday 26th , it will be great if I could have few days
+# before that to interpreted the results.
+#
+# Don`t worry about x-ray findings and surgical site check columns, and I have
+# included all detected complications in the complication column. Also, we will
+# not analyse return to work for now.
+#
+# You might be aware that we have submitted the trial protocol for publication
+# in a peer-reviewed Journal (Trials), one of the reviewers has commented on our
+# statistical plan, as very basic and claimed that our sample size calculation
+# is wrong (146 instead of 140). That might be to my beginner level in
+# statistics but I used STATA to calculate the sample size and I think she made
+# an error in adding the 20% drop-out !. Of note, she is not a statistician. I
+# would appreciate if you could please read her comments and feel free to adjust
+# the statistical analysis plan as you think yourself appropriate. Any way we
+# need a new calculation as the total number now is 160. I will be resubmitting
+# to the journal once I have your replay, this is not urgent.
+#
+# I have attached the full trial protocol for your reference if you need it,
+# also attached the statistical protocol plan and the reviewer comments in one
+# document.
+# 
+
+
+# Jan 31
+
+# To finalize calculation of sample size, as we discussed we would like to
+# achieve our target by May 2020 due to many reasons. This is my
+# finalcalculation: To detect a clinically significant 10 points difference on
+# the Olerud and Molander Ankle Score (OMAS) at 6 weeks with a standard
+# deviation of 19. Alpha = 0.05 and β=0.20 (80% power), two-sided test and a
+# maximum loss of follow up of 20 %, a sample size of 70 per each group is
+# necessary.
+
+# Dec 16
+
+# We are currently halfway through a randomised controlled study comparing
+# post-operative management of ankle fractures. We are approaching our target
+# number at UHW, and we hope to start recruitment in CUH and GUH soon as we
+# already granted ethical approval from CUH and GUH.
+#
+# We would appreciate your support and specifically, statistical support for
+# this research. Further help in providing a research nurse to help the research
+# team at CUH will be appreciated.
+#
+# You can find more details regarding the trial methodology in this link
+# http://www.isrctn.com/ISRCTN76410775
+
+                                                                                                                                                                             Given the fact that I don`t want to increase sample size more than 140, I have two questions;
+                                                                                                                                                                             1.	Is adding 20% loss of follow up necessary in the initial calculation as we will include everyone who is randomised to the analysis (ITT)?
+                                                                                                                                                                               2.	I can increase the power to 87% by removing the 20% loss of follow up in the initial calculation. Which one is more important ?
+                                                                                                                                                                               
---
+++ scripts/01_data.R
@@ -0,0 +1,252 @@
+
+  library(readxl)
+  library(tidyverse)
+  library(testthat)
+  library(viridis)
+
+# Data -------------------------------------------------------------------------
+
+  data <- read_excel("data/Ankle Trial Anonymous Data.xlsx")
+
+# names(data)
+# lapply(data, class)
+# lapply(data, table)
+# lapply(data, function(x) table(is.na(x)))
+
+  names(data) <- gsub(
+    "\\.\\.\\.|\\.\\.|\\.", "_", make.names(tolower(names(data)))
+  )
+  
+# Remove blank rows and columns
+  data <- data[, unlist(lapply(data, function(x) !all(is.na(x))))] # blank cols
+  data <- data[rowSums(is.na(data)) != ncol(data),] # Remove blank rows
+  data <- filter(data, !is.na(randmization))
+  
+# Inspect data #################################################################
+  
+  # library(summarytools)
+  # view(dfSummary(data))  
+  
+# Check and clean variables ####################################################
+  
+# ID, Arm, Consent ----
+  data <- rename(data, arm = randmization)
+  data$id <- data$arm
+  data$id <- gsub("\\D+", "", data$id)
+  
+  expect_equal(length(unique(data$id)), nrow(data)) # Are IDs unique, 1 row per
+  
+  data$arm <- gsub("\\d+", "", data$arm) %>%
+    gsub("\\.", "", .) %>%
+    gsub("_", "", .) %>%
+    gsub("^\\s+|\\s+$", "", .) %>%
+    factor(levels = c("Cast", "Boot"))
+  
+# table(data$arm)
+  expect_equal(table(data$arm)[[1]], table(data$arm)[[2]])               # QUERY
+# This had a block randomization with block size of 20. "Lucky" to get exactly
+# 80:80   
+  
+# Consent
+  
+  expect_equal(table(data$consent)[[1]], nrow(data)) # All obs have consent
+  
+# Protocol violations and loss to follow-up ----
+  
+# table(data$violation_and_ltf)
+  data$violation[data$violation_and_ltf == 15] <- 1
+  data$ltf[data$violation_and_ltf == 12] <- 1
+  
+
+  
+# Outcomes ----
+  
+# Primary ----
+# Olerud-Molander Ankle Score (OMAS). This score ranges from 0 to 100,
+# with 100 representing the normal ankle function 
+  
+# names(select(data, contains("omas")))
+  
+  data <- rename(
+    data, 
+    omas_01_14d = omas_score_2_weeks2, 
+    omas_02_42d = omas_score_6_weeks, 
+    omas_03_90d = omas_score_3_months, 
+    omas_04_180d = omas_score_6_months, 
+    omas_05_365d = omas_score_1_year
+    )
+  
+  sum_plot <- function(sub, ...){
+    select(data, arm, contains(sub)) %>%
+      gather(time, val, -arm) %>%
+      ggplot(aes(x = val, fill = arm)) +
+      geom_density(alpha = 0.5) +
+      facet_wrap(~time, ncol = 1) +
+      scale_fill_viridis("", discrete = TRUE, begin = 0.5,
+                         end = 0.8, option = "inferno", direction = -1) +
+      theme(panel.background = element_rect(fill = "grey20"),
+            panel.grid = element_blank(),
+            strip.background = element_rect(fill = "grey20"),
+            strip.text = element_text(color = "white")) +
+      ylab("Density") +
+      xlab("Value")
+  }
+  
+  # sum_plot("omas")
+
+  # 
+  # map(
+  #   select(data, arm, contains("omas")), 
+  #   function(x)table(is.na(x))
+  # ) # Lots of missing as trial progresses
+  
+  
+# Secondary ----
+  
+# Complication rate (infection and fixation failure) ----
+                                                 # Not enough of these to matter
+
+  # table(data$complication)
+  # table(data$complications_note)
+  # map(
+  #   select(data, contains("complication")), 
+  #   function(x)table(is.na(x))
+  # )
+  # 
+  # select(data, id, arm, contains("complication")) %>%
+  #   filter(!is.na(complication) | !is.na(complications_note)) %>%
+  #   View()
+  
+  # These need to be split out. 
+  
+  data$infection <- NA
+  # Picks up any complication with a 1, but not 11.   
+  # table(data$complication)  
+  # table(data$complication[grepl("1(?!1|$)|^1$", data$complication, perl = T)])  
+  data$infection[grepl("1(?!1|$)|^1$", data$complication, perl = T)] <- 1 
+  # One issue here is that it's not clear who was lost to follow up yet, and so
+  # may have had a complication that wasn't picked up                    # QUERY  
+  
+  # data$nonunion <- NA
+  # table(data$complication[grepl("10", data$complication)]) 
+  # data$nonunion[grepl("10", data$complication)] <- 1           # No non-unions
+
+  
+  # Total arc of ankle motion ----
+  # (plantar flexion and dorsal-flection) measured in degrees using a goniometer 
+  
+  # names(select(data, contains("arc")))
+  # names(select(data, contains("ankle")))
+  
+  
+  data <- rename(
+    data, 
+    ankle_01_14d = total_ankle_arc, 
+    ankle_02_42d = total_ankle_arc_35, 
+    ankle_03_90d = total_ankle_arc_2, 
+    ankle_04_180d = total_arc, 
+    ankle_05_365d = total_ankle_arc_62
+  )
+  
+  # sum_plot("ankle_")
+  
+  # map(
+  #   select(data, arm, contains("ankle_")),
+  #   function(x)table(is.na(x))
+  # ) # Lots of missing as trial progresses
+  
+  
+# RAND 36-Item Short Form Survey (SF-36) scoring ----
+  
+  # names(select(data, starts_with("sf_")))
+  
+  data <- rename(
+    data, 
+    sf_ph_01_14d = sf_36_physical_health_2_weeks, 
+    sf_ph_02_42d = sf_36_ph_39, 
+    sf_ph_03_90d = sf_36_ph2, 
+    sf_ph_04_180d = sf_36_ph_57, 
+    sf_ph_05_365d = sf_36_ph_2
+  )
+  
+  # sum_plot("sf_ph")
+  
+  data <- rename(
+    data, 
+    sf_mh_01_14d = sf_36_mental_health, 
+    sf_mh_02_42d = sf_36_mh2, 
+    sf_mh_03_90d = sf_36_mh_50, 
+    sf_mh_04_180d = sf_36_6_mh, 
+    sf_mh_05_365d = sf_36_mh_65
+  )
+  
+  # sum_plot("sf_mh")
+  
+  
+# The time needed to return to work in days ----
+  
+  # names(select(data, starts_with("return")))
+  
+  data <- rename(
+    data, 
+    rtw_02_42d = return_to_work_37, 
+    rtw_03_90d = return_to_work_2, 
+    rtw_04_180d = return_to_work_55, 
+  )
+  
+  data$rtw_03_90d <- as.numeric(gsub("\\D+", "", data$rtw_03_90d)) 
+  data$rtw_04_180d[data$rtw_04_180d > 5 & !is.na(data$rtw_04_180d)] <- 1
+  
+  tar <- grepl("rtw_", names(data))
+  data[tar] <- map(
+    data[tar], 
+    factor, 
+    levels = 0:4, 
+    labels = c("No", "Yes", "Not Yet", "Retired", "Unemployed")
+  )
+  
+  # map(data[tar], table)
+  
+  # select(data, id, arm, contains("rtw_")) %>%
+  #   gather(time, val, -arm, - id) %>%
+  #   filter(val %in% c("No", "Yes")) %>%
+  #   ggplot(aes(y = val, x = time, color = id, group = id)) +
+  #   geom_line(alpha = 1, 
+  #             position = position_jitter(width = 0.1, height = 0.1)) +
+  #   facet_wrap(~arm) +
+  #   scale_color_viridis(guide = FALSE, discrete = TRUE) +
+  #   theme(panel.background = element_rect(fill = "grey20"),
+  #         panel.grid = element_blank(),
+  #         strip.background = element_rect(fill = "grey20"),
+  #         strip.text = element_text(color = "white")) +
+  #   ylab("Density") +
+  #   xlab("Value")
+
+  
+  
+# Postoperative hospitalisation length in days ----
+  data$date_of_discharge <- as.POSIXct(
+    as.numeric(data$date_of_discharge) * 60 * 60 * 24,
+    origin = "1899-12-30"
+    )
+  
+  data$los <- difftime(
+    data$date_of_discharge, data$date_of_surgery, units = "days"
+    ) %>%
+    as.numeric()
+  
+  # ggplot(data, aes(x = arm, fill = factor(los))) +
+  #   geom_bar(position = "fill") +
+  #   scale_fill_viridis("", discrete = TRUE, option = "inferno", begin = 0.1, 
+  #                      direction = -1) +
+  #   theme(panel.background = element_rect(fill = "grey20"),
+  #         panel.grid = element_blank(),
+  #         strip.background = element_rect(fill = "grey20"),
+  #         strip.text = element_text(color = "white")) +
+  #   ylab("Proportion") +
+  #   xlab("")
+  
+# Save data ####################################################################
+  save(data, file = "data.RData")
+  rm(list = ls())
+  load("data.RData")
